2993848bb70392ce88040642405265ee84ee2c2c
ANDRE24-f <rikhert.andrey@gmail.com>
2025-01-18 13:33:55 +0700

    asdasd

--- Assets/Scripts/StakanWater.cs
+++ Assets/Scripts/StakanWater.cs
@@ -7,12 +7,15 @@ public class StakanWater : MonoBehaviour
     [SerializeField] GameObject water_part;
     [SerializeField] GameObject water_ist;
     [SerializeField] GameObject water_;
+    bool wat;
 
     void Update()
     {
-        if (this.transform.rotation.x > 0.5f|| this.transform.rotation.x > 0.5f)
+        if (this.transform.rotation.x > 0.5f && water > 0 || this.transform.rotation.x > 0.5f&&water>0)
         {
             water_part.SetActive(true);
+            water -= 10 * Time.deltaTime;
+            water_.transform.localScale = new Vector3((65 + 35 * water / 100) / 100, water / 100, (65 + 35 * water / 100) / 100);
         }
         else
         {
@@ -20,20 +23,31 @@ public class StakanWater : MonoBehaviour
         }
         if (water_ist.tag == "water"&&water<100f)
         {
-            water += 5*Time.deltaTime;
+            water += 10*Time.deltaTime;
         }
-        if (water >= 100)
+        if (water > 100)
         {
             water = 100;
         }
-        if (water < 70) { water_.transform.localScale = new Vector3(water, water, 2); }
-        else
+        if(water < 0)
         {
-            water_.transform.localScale = new Vector3(water, water, water);
+            water = 0;
         }
+        water_.transform.localScale = new Vector3((65 + 35 * water / 100) / 100, water / 100, (65 + 35 * water / 100) / 100);
     }
     private void OnTriggerEnter(Collider other)
     {
-        water_ist = other.gameObject;
+        if (other.tag == "water")
+        {
+            water_ist = other.gameObject;
+        }
+        
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "water")
+        {
+            water_ist = null;
+        }
     }
 }
