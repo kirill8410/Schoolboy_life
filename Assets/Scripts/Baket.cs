19b44a5a0544b4137e1aa8a659c629a3050ef2f3
itcube22 <104897682+itcube22@users.noreply.github.com>
2025-01-18 18:32:57 +0700

    asda

--- Assets/Scripts/Baket.cs
+++ Assets/Scripts/Baket.cs
@@ -3,19 +3,17 @@ using UnityEngine;
 public class Baket : MonoBehaviour
 {
     [SerializeField] int clothes;
-    [SerializeField] MonoBehaviour script;
-    GameObject TargetObj; //не забудь перетащить в инспекторе сюда нужный обьект
-    private SomeMonoBehavior _actionTarget; //замени SomeMonoBehavior  на название скрипта
+    [SerializeField] GameObject TargetObj;
+    private Quest _actionTarget; //замени SomeMonoBehavior  на название скрипта
 
     public void Start()
     {
-        _actionTarget = TargetObj.GetComponent<SomeMonoBehavior>();
+        _actionTarget = TargetObj.GetComponent<Quest>();
     }
     private void Update()
     {
         if (clothes == 0)
         {
-            _actionTarget.Method();
         }
     }
     private void OnTriggerEnter(Collider other)
