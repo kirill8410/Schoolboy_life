696f115b4205092b4f05be93f1bebcecb0b9f5a2
itcube22 <104897682+itcube22@users.noreply.github.com>
2025-01-31 10:45:51 +0700

    a

--- Assets/Scripts/Mother.cs
+++ Assets/Scripts/Mother.cs
@@ -5,13 +5,15 @@ using UnityEngine.XR.Interaction.Toolkit.Interactors;
 
 public class Mother : MonoBehaviour
 {
+    [SerializeField] NPCConversation[] _NCs;
     NPCConversation _NC;
     QuestManager _QM;
     NearFarInteractor[] _NF;
+    bool[] _isDialogues = new bool[2] {false, false} ;
 
     private void Start()
     {
-        _NC = GetComponentInChildren<NPCConversation>();
+        _NC = _NCs[0];
         _QM = FindFirstObjectByType<QuestManager>();
     }
 
@@ -19,7 +21,7 @@ public class Mother : MonoBehaviour
     {
         if (other.GetComponent<XROrigin>() != null)
         {
-            if (Resources.Load<Quest>("SO/Quest1").isComplite)
+            if (Resources.Load<Quest>("SO/Quest1").isComplite && !_isDialogues[0])
             {
                 _NF = GameObject.FindObjectsByType<NearFarInteractor>(FindObjectsSortMode.None);
                 foreach (NearFarInteractor N in _NF)
@@ -27,11 +29,23 @@ public class Mother : MonoBehaviour
                     N.enableFarCasting = true;
                 }
                 ConversationManager.Instance.StartConversation(_NC);
+                _isDialogues[0] = true;
+                _NC = _NCs[1];
+            }
+            if (Resources.Load<Quest>("SO/Quest2").isComplite && Resources.Load<Quest>("SO/Quest3").isComplite && !_isDialogues[1])
+            {
+                _NF = GameObject.FindObjectsByType<NearFarInteractor>(FindObjectsSortMode.None);
+                foreach (NearFarInteractor N in _NF)
+                {
+                    N.enableFarCasting = true;
+                }
+                ConversationManager.Instance.StartConversation(_NC);
+                _isDialogues[1] = true;
             }
         }
     }
 
-    public void AddQuest()
+    public void AddQuestOne()
     {
         _QM.AddQuest(Resources.Load<Quest>("SO/Quest2"));
         _QM.AddQuest(Resources.Load<Quest>("SO/Quest3"));
